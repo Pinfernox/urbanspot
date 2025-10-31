25425ca1c97deb72a46fa2ef084d26d344fcf044
AlejandroCC <alcaca172@uma.es>
2025-10-31 13:51:44 +0100

    fix: Añadir dependencia 'ol' (OpenLayers) al package.json

--- urbanspot/src/auth.ts
+++ urbanspot/src/auth.ts
@@ -3,19 +3,20 @@ import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { 
-  handlers, // Esto es para tus rutas API (ej. /api/auth/...)
-  auth,     // Esta es la función que usará el middleware
+  handlers,
+  auth,
   signIn, 
   signOut 
 } = NextAuth({
   providers: [
+    // ¡COMENTA ESTA LÍNEA!
+    /*
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+    */
   ],
-  
-
   pages: {
     signIn: "/login",
   },
